d2933784e1c717969e280f5bbaa3e71c6af21dd3
mathe1 <mathe@mathesoft.de>
2020-06-26 02:40:21 +0200

    Detect phone disconnected
    Log the disconnection period and alert with sound.

--- tracker.js
+++ tracker.js
@@ -3,16 +3,20 @@
 // Edit the Classnames when script don't work
 // OnlineLabelClass is there in the headline under the contact's name
 // ContactNameClass is right from the contact's profile picture in the headline
+
 var OnlineLabelClass = "_3-cMa"; //until 10.06.2020 "O90ur"; 
 var ContactNameClass = "_33QME"; //until 10.06.2020 "_5SiUq";
 var ToolsClass = "_3nq_A"; //div for right side at 3dots
 var SeenClassContainer = "_13opk"; //highlighted contact.. has seen the message
 var SeenClass = "_2RFeE"; //checks are blue
+var phonestatusClass = "_2vbYK"; // shows the sign and yellow "disconnected" hint 
 
 var isOnline = 0;
 var logged = 0;
+var phonealert = -1;
 
 var last_time;
+var last_time_backup;
 var offtime = new Date();
 var offtimelabel="Init";
 
@@ -92,17 +96,33 @@ setInterval(function() {
   var date = new Date();
   var time = ('0'+date.getHours()).slice(-2) + ':' + ('0'+date.getMinutes()).slice(-2) + ':' + ('0'+date.getSeconds()).slice(-2);
   
+  //check phone status maybe disconnected and log that 
+  var phonestatus=document.getElementsByClassName(phonestatusClass)[0]; 
+  if (phonestatus) if (phonestatus.firstElementChild.dataset.icon=='alert-phone') {
+    if (phonealert < 1) {
+     _play('alert'); 
+     if (phonealert < 0) { 
+       last_time_backup = last_time;
+       consolelog(time+' ⚠️ phone disconnected'); 
+       offtime = new Date();
+       offtimelabel = ('0'+offtime.getHours()).slice(-2) + ':' + ('0'+offtime.getMinutes()).slice(-2) + ':' + ('0'+offtime.getSeconds()).slice(-2);
+       last_time = offtime;
+     }
+     phonealert = 10;
+    } else phonealert--; 
+    SetStatus('⚠️ Disconnected '+offtimelabel+get_time_diff(2));
+    return
+  }
+  if (phonealert>0) { consolelog(time+' ⚠️ alert finished'+get_time_diff(1)); phonealert = -1; offtime = last_time_backup; }
+  
   var msgcheck=document.getElementsByClassName(SeenClassContainer);
-  if (msgcheck.length>0 && msg_seen==0) {
+  if (msgcheck.length>0) {
     if (msgcheck[0].getElementsByClassName(SeenClass).length>0) {
+     if (msg_seen==0) _play("note");
      msg_seen=1;
-     _play("note");
-    }
+    } 
+    else msg_seen=0;
   } 
-  else 
-   if (msgcheck.length==0) msg_seen=0;
-   else
-     if (msgcheck[0].getElementsByClassName(SeenClass).length==0) msg_seen=0;
       
   try {
    var ctc = document.getElementsByClassName(ContactNameClass);
