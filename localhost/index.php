82631ac416e0cb6a85fbb6ab51c70a22b8d06c14
mathe1 <mathe@mathesoft.de>
2020-06-29 16:32:20 +0200

    better fix
    before the logic worked on "php tryit editor", but not at server ?!

--- localhost/index.php
+++ localhost/index.php
@@ -1,7 +1,7 @@
 <?php
 $hash="711vJy&";
 $u=parse_url($_SERVER["REQUEST_URI"], PHP_URL_QUERY);
-if (!empty($u) and strpos($u,$hash)) {
+if (!empty($u) and strpos($u,$hash)!==false) {
  $f = fopen("track".date('ymd').".txt","a");
  fwrite($f,"> ".html_entity_decode(urldecode(str_replace($hash, "", $u)),ENT_NOQUOTES|ENT_HTML5,"UTF-8")."\n");
  fclose($f);
