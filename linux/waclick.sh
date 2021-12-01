d0a476e0052ebacc94521e282093e931ff131974
mathe1 <mathe@mathesoft.de>
2021-12-01 17:38:30 +0100

    shell script
    You may remove the comments ( starts with ## )
    The script runs infinity until the active window is not whatsapp.

---
+++ linux/waclick.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+WID=$(xdotool search WhatsApp | head -n1) ## find the browser window where whatsapp is running
+xdotool windowactivate $WID ## bring the window to front
+while [ $(xdotool getactivewindow) = $WID ]
+do 
+  xdotool mousemove 405 777 click 1 ## in the feed, but edit your own point
+  sleep 20 ## secounds
+  xdotool mousemove 683 832 click 1 ## input field, but edit your own point
+  sleep 20
+done
