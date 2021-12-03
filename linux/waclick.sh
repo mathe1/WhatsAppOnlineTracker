03a12e299b4eb406e9e0ef6d7f418536a7682c43
mathe1 <mathe@mathesoft.de>
2021-12-03 20:11:47 +0100

    Fix "while" syntax
    Bash scripting is new for me

--- linux/waclick.sh
+++ linux/waclick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 WID=$(xdotool search WhatsApp | head -n1) ## find the browser window where whatsapp is running
 xdotool windowactivate $WID ## bring the window to front
-while (( $(xdotool getactivewindow) == $WID ))
+while [ $(xdotool getactivewindow) -eq $WID ]
 do 
   xdotool mousemove 405 777 click 1 ## in the feed, but edit your own point
   sleep 20 ## secounds
